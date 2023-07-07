9f1ff6c8dffa0e9ff4d0a093db5eea7e7a243492
Billy Lee Wong <leewbr10@gmail.com>
2023-07-07 22:46:24 +0000

    adding initial page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <h1>Não deixe de falar as palavras deste Livro da Lei e de meditar nelas de dia e de noite, <br/>
+        para que você cumpra fielmente tudo o que nele está escrito. <br/>
+        Só então os seus caminhos prosperarão e você será bem sucedido. <br/>
+        Josué 1:8</h1>
+}
+
+export default Home;
