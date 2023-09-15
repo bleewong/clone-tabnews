ada6bfa0719e7be0f6d9f5d376ef0dcab5b0df5f
Billy Lee Wong <leewbr10@gmail.com>
2023-09-15 19:16:39 +0000

    adding script `lint:check`and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação de um projeto do zero
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 function Home() {
-    return <h1>Não deixe de falar as palavras deste Livro da Lei e de meditar nelas de dia e de noite, <br/>
-        para que você cumpra fielmente tudo o que nele está escrito. <br/>
-        Só então os seus caminhos prosperarão e você será bem sucedido. <br/>
-        Josué 1:8</h1>
+  return (
+    <h1>
+      Não deixe de falar as palavras deste Livro da Lei e de meditar nelas de
+      dia e de noite, <br />
+      para que você cumpra fielmente tudo o que nele está escrito. <br />
+      Só então os seus caminhos prosperarão e você será bem sucedido. <br />
+      Josué 1:8
+    </h1>
+  );
+}
+
+function test() {
+  console.log("test");
+}
+
+function test2() {
+  console.log("identação errada!!!");
 }
 
 export default Home;
