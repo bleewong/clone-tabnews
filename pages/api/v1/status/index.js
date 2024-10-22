191821c77ef5627c2903ea65b2686fa94231c0ae
Billy Lee Wong <leewbr10@gmail.com>
2024-10-22 17:40:17 +0000

    add `jsconfig.js` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("Select 1 + 1 as sum;");
