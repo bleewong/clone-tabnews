7e1975b1bd349e271371564b14ecad26f2343a28
Billy Lee Wong <leewbr10@gmail.com>
2024-10-27 18:07:24 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,39 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("Select 1 + 1 as sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ message: "alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  let databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  let databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  console.log(databaseName);
+  const databaseOpenedConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  console.log(databaseOpenedConnectionResult.rows);
+
+  const databaseOpenedConnectionValue =
+    databaseOpenedConnectionResult.rows[0].count;
+  console.log(databaseOpenedConnectionValue);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionValue,
+      },
+    },
+  });
 }
 
 export default status;
